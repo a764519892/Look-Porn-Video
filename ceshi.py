56d8bc74612140fc12542959ae0e3eb66909c78b
a764519892 <31115382+a764519892@users.noreply.github.com>
2021-08-10 09:36:45 -0500

    Add files via upload
    Add new addresses every day。

---
+++ ceshi.py
@@ -0,0 +1,344 @@
+# -*- coding: utf-8 -*-
+
+from lxml import etree
+
+text= '''<!DOCTYPE HTML PUBliC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3c.org/TR/1999/REC-html401-19991224/loose.dtd">
+<HTML xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>国产精品</title>
+<meta name="keywords" content="国产精品">
+<meta name="description" content="国产精品">
+<script type="text/javascript" src="/js/domains_index.js"></script>
+<script type="text/javascript" src="/js/getdz.js"></script>
+<link type="text/css" rel="stylesheet" href="/Tpl/defalut/css0518.css">
+<script type="text/javascript" src="/js/jquery.js?v=1"></script>
+<link rel="preconnect" href="https://daoen888.com" crossorigin>
+<link rel="preconnect" href="https://p.pstatp.com" crossorigin>
+<link rel="preconnect" href="https://cdn.jsdelivr.net" crossorigin>
+
+</head>
+<body>
+<script type="text/javascript" src="/js/menu.js?v=4"></script>
+ 
+<script type="text/javascript" src="/js/nfhry32_78.js"></script>
+<script type="text/javascript">
+function IsBan3() {
+    var userAgentInfo = navigator.userAgent;
+    var Agents = ["UCBrowser"];
+    var flag = false;
+    for (var v = 0; v < Agents.length; v++) {
+        if (userAgentInfo.indexOf(Agents[v]) > 0) {
+            flag = true;
+            break;
+        }
+    }
+    return flag;
+}
+</script>
+<script type="text/javascript" src="/js/uc.js"></script>
+<div class="wrap mt21">
+	<div class="box cat_pos clearfix">
+<span class="cat_pos_l">您的位置：<a style="font-size: 20px;" href='/'>首頁</a>&nbsp;&nbsp;&raquo;&nbsp;&nbsp;<a style="font-size: 20px;" href='/Html/60/' >国产精品</a>
+    <br><br>
+    <a style="font-size: 25px;color: red;" href="javascript:void(0);" onclick="v27();" ><b onclick="tj('list','text','1','v27','1');">美女真人百家乐</b></a>
+    &nbsp;&nbsp;&nbsp;&nbsp;<a style="font-size: 25px;color: red;" href="javascript:void(0);" onclick="yl();" ><b onclick="tj('list','text','2','yl','1');">新人送豪礼</b></a>
+    &nbsp;&nbsp;&nbsp;&nbsp;<a style="font-size: 25px;color: red;" href="javascript:void(0);" onclick="zsyl();" ><b onclick="tj('list','text','3','zs','1');">天天领红包</b></a>
+    &nbsp;&nbsp;&nbsp;&nbsp;<a style="font-size: 25px;color: red;" href="javascript:void(0);" onclick="jjqpJump();" ><b onclick="tj('list','text','4','jjqp','1');">棋牌万人在线</b></a>
+</span>
+	</div></div>
+<div class="wrap mt21">
+	<div class="box movie2_list gfzgfdbbf">
+		<ul>
+  
+<li><a href="/Html/87/20733.html" target="_blank">
+    <script>
+        var piclocal = get_local("/20210413/87/20016/20016.mp4.jpg");
+        var piclazy = get_lazy();
+        document.write('<img class="lazyLoad" src="' + piclazy + '" data-original="' + piclocal + '" title="CDM-20325"/>' );							
+    </script> 
+    <!--[if lt IE 9 ]><span class="bg_top"><![endif]--><span class="movie_date">2021-04-13<!--[if lt IE 9 ]></span><span class="bg_tail"></span><![endif]--></span><h3>CDM-20325</h3></a></li>
+        <li><a href="/Html/109/20727.html" target="_blank">
+    <script>
+        var piclocal = get_local("/20210413/109/50020/50020.mp4.jpg");
+        var piclazy = get_lazy();
+        document.write('<img class="lazyLoad" src="' + piclazy + '" data-original="' + piclocal + '" title="I_Love_My_Man"/>' );							
+    </script> 
+    <!--[if lt IE 9 ]><span class="bg_top"><![endif]--><span class="movie_date">2021-04-13<!--[if lt IE 9 ]></span><span class="bg_tail"></span><![endif]--></span><h3>I_Love_My_Man</h3></a></li>
+        <li><a href="/Html/93/20723.html" target="_blank">
+    <script>
+        var piclocal = get_local("/20210412/93/20089/20089.mp4.jpg");
+        var piclazy = get_lazy();
+        document.write('<img class="lazyLoad" src="' + piclazy + '" data-original="' + piclocal + '" title="鲍鱼淫水可见，啪啪细节完美呈现，清晰度吊打全场探花"/>' );							
+    </script> 
+    <!--[if lt IE 9 ]><span class="bg_top"><![endif]--><span class="movie_date">2021-04-12<!--[if lt IE 9 ]></span><span class="bg_tail"></span><![endif]--></span><h3>鲍鱼淫水可见，啪啪细节完美呈现，清晰度吊打全场探花</h3></a></li>
+        <li><a href="/Html/91/20722.html" target="_blank">
+    <script>
+        var piclocal = get_local("/20210412/91/910019/910019.mp4.jpg");
+        var piclazy = get_lazy();
+        document.write('<img class="lazyLoad" src="' + piclazy + '" data-original="' + piclocal + '" title="网红御酱首发私人订制视频情色妖狐尾大玩具深插粉肉浪穴淫语自嗨"/>' );							
+    </script> 
+    <!--[if lt IE 9 ]><span class="bg_top"><![endif]--><span class="movie_date">2021-04-12<!--[if lt IE 9 ]></span><span class="bg_tail"></span><![endif]--></span><h3>网红御酱首发私人订制视频情色妖狐尾大玩具深插粉肉浪穴淫语自嗨</h3></a></li>
+    <script>
+        var pgglist = pgglists();
+        document.writeln('<li><a href="'+listurls[l]+'" target="_blank" >');
+                document.write('<img src="' + pgglist + '" title="'+listtext[l]+'"/>' );
+                document.writeln('<h3>'+listtext[l]+' </h3>');						
+            </script>
+            <span class="movie_date">2021-04-12</span></a></li>    <li><a href="/Html/89/20721.html" target="_blank">
+    <script>
+        var piclocal = get_local("/20210412/89/20088/20088.mp4.jpg");
+        var piclazy = get_lazy();
+        document.write('<img class="lazyLoad" src="' + piclazy + '" data-original="' + piclocal + '" title="長腿妮妮誤闖KTV包房遭五男人輪番狂轟亂肏-艾絲出差訂錯房三人同住被兩根肉棒填塞等～03"/>' );							
+    </script> 
+    <!--[if lt IE 9 ]><span class="bg_top"><![endif]--><span class="movie_date">2021-04-12<!--[if lt IE 9 ]></span><span class="bg_tail"></span><![endif]--></span><h3>長腿妮妮誤闖KTV包房遭五男人輪番狂轟亂肏-艾絲出差訂錯房三人同住被兩根肉棒填塞等～03</h3></a></li>
+        <li style="line-height: 0px;font-size:0px;width: 100% !important;margin-top:0px;">
+            <a href="javascript:void(0);" onclick="v27();"><img border="0" src="https://p.pstatp.com/origin/fe9e0003db21315c5b9d" 
+                onclick="tj('list','hf','1','v27','1');"  style="width: 920px;height: 70px; margin: auto;margin-bottom: 10px;">
+                <img border="0" src="https://p.pstatp.com/origin/fe9e0003db21315c5b9d" 
+                onclick="tj('list','hf','1','v27','1');"  style="width: 920px;height: 70px; margin: auto;margin-bottom: 10px;">
+                <img border="0" src="https://p.pstatp.com/origin/fe9e0003db21315c5b9d" 
+                onclick="tj('list','hf','1','v27','1');"  style="width: 920px;height: 70px; margin: auto;margin-bottom: 10px;">
+            </a>
+        </li><li><a href="/Html/88/20720.html" target="_blank">
+    <script>
+        var piclocal = get_local("/20210412/88/20087/20087.mp4.jpg");
+        var piclazy = get_lazy();
+        document.write('<img class="lazyLoad" src="' + piclazy + '" data-original="' + piclocal + '" title="長腿國模旅館全裸私拍不同姿勢展示美妙胴體攝影揩油掰玩粉穴"/>' );							
+    </script> 
+    <!--[if lt IE 9 ]><span class="bg_top"><![endif]--><span class="movie_date">2021-04-12<!--[if lt IE 9 ]></span><span class="bg_tail"></span><![endif]--></span><h3>長腿國模旅館全裸私拍不同姿勢展示美妙胴體攝影揩油掰玩粉穴</h3></a></li>
+        <li><a href="/Html/87/20719.html" target="_blank">
+    <script>
+        var piclocal = get_local("/20210412/87/20086/20086.mp4.jpg");
+        var piclazy = get_lazy();
+        document.write('<img class="lazyLoad" src="' + piclazy + '" data-original="' + piclocal + '" title="车展偷窥车模奶子"/>' );							
+    </script> 
+    <!--[if lt IE 9 ]><span class="bg_top"><![endif]--><span class="movie_date">2021-04-12<!--[if lt IE 9 ]></span><span class="bg_tail"></span><![endif]--></span><h3>车展偷窥车模奶子</h3></a></li>
+        <li><a href="/Html/109/20713.html" target="_blank">
+    <script>
+        var piclocal = get_local("/20210412/109/50019/50019.mp4.jpg");
+        var piclazy = get_lazy();
+        document.write('<img class="lazyLoad" src="' + piclazy + '" data-original="' + piclocal + '" title="Hyangdan_-Directors_Cut"/>' );							
+    </script> 
+    <!--[if lt IE 9 ]><span class="bg_top"><![endif]--><span class="movie_date">2021-04-12<!--[if lt IE 9 ]></span><span class="bg_tail"></span><![endif]--></span><h3>Hyangdan_-Directors_Cut</h3></a></li>
+        <li><a href="/Html/93/20709.html" target="_blank">
+    <script>
+        var piclocal = get_local("/20210411/93/20085/20085.mp4.jpg");
+        var piclazy = get_lazy();
+        document.write('<img class="lazyLoad" src="' + piclazy + '" data-original="' + piclocal + '" title="超骚人妻与同事海外工作偷情自拍~第二部公寓内射篇和老外搞多了妹子逼逼目测貌似得过病"/>' );							
+    </script> 
+    <!--[if lt IE 9 ]><span class="bg_top"><![endif]--><span class="movie_date">2021-04-11<!--[if lt IE 9 ]></span><span class="bg_tail"></span><![endif]--></span><h3>超骚人妻与同事海外工作偷情自拍~第二部公寓内射篇和老外搞多了妹子逼逼目测貌似得过病</h3></a></li>
+        <li><a href="/Html/90/20707.html" target="_blank">
+    <script>
+        var piclocal = get_local("/20210411/90/20084/20084.mp4.jpg");
+        var piclazy = get_lazy();
+        document.write('<img class="lazyLoad" src="' + piclazy + '" data-original="' + piclocal + '" title="超诱惑妝妝xBetty纯洁的修女姿色太诱人女学生霸王硬上弓黑丝女神的堕落"/>' );							
+    </script> 
+    <!--[if lt IE 9 ]><span class="bg_top"><![endif]--><span class="movie_date">2021-04-11<!--[if lt IE 9 ]></span><span class="bg_tail"></span><![endif]--></span><h3>超诱惑妝妝xBetty纯洁的修女姿色太诱人女学生霸王硬上弓黑丝女神的堕落</h3></a></li>
+        <li><a href="/Html/89/20706.html" target="_blank">
+    <script>
+        var piclocal = get_local("/20210411/89/20083/20083.mp4.jpg");
+        var piclazy = get_lazy();
+        document.write('<img class="lazyLoad" src="' + piclazy + '" data-original="' + piclocal + '" title="超级白嫩的少妇-叫干嘛就干吗很爽-干累了休息一下在继续舔蛋蛋要化了"/>' );							
+    </script> 
+    <!--[if lt IE 9 ]><span class="bg_top"><![endif]--><span class="movie_date">2021-04-11<!--[if lt IE 9 ]></span><span class="bg_tail"></span><![endif]--></span><h3>超级白嫩的少妇-叫干嘛就干吗很爽-干累了休息一下在继续舔蛋蛋要化了</h3></a></li>
+        <li><a href="/Html/88/20705.html" target="_blank">
+    <script>
+        var piclocal = get_local("/20210411/88/20082/20082.mp4.jpg");
+        var piclazy = get_lazy();
+        document.write('<img class="lazyLoad" src="' + piclazy + '" data-original="' + piclocal + '" title="超有气质美女在家自慰卖肉给富二代-自成当过空姐和护士-制服很诱惑"/>' );							
+    </script> 
+    <!--[if lt IE 9 ]><span class="bg_top"><![endif]--><span class="movie_date">2021-04-11<!--[if lt IE 9 ]></span><span class="bg_tail"></span><![endif]--></span><h3>超有气质美女在家自慰卖肉给富二代-自成当过空姐和护士-制服很诱惑</h3></a></li>
+        
+			
+		</ul>
+		<div class="clear"></div>
+		<div class="pagination">
+
+		<strong>共2206部&nbsp;69/184</strong><a href="/Html/60/index.html" class="prev disabled" data="p-0">首页</a>&nbsp;<a href="/Html/60/index-68.html" class="prev" data="p-68">上一页</a>&nbsp;<a href="/Html/60/index-66.html" data="p-66">66</a>&nbsp;<a href="/Html/60/index-67.html" data="p-67">67</a>&nbsp;<a href="/Html/60/index-68.html" data="p-68">68</a>&nbsp;<span class="current">69</span>&nbsp;<a href="/Html/60/index-70.html" data="p-70">70</a>&nbsp;<a href="/Html/60/index-71.html" data="p-71">71</a>&nbsp;<a href="/Html/60/index-72.html" data="p-72">72</a>&nbsp;<a href="/Html/60/index-70.html" class="next pagegbk" data="p-70">下一页</a>&nbsp;<a href="/Html/60/index-184.html" class="next pagegbk" data="p-184">尾页</a>			</div></div>
+</div>
+
+
+<script type="text/javascript" src="/js/xgrghru981_23.js"></script>
+<div id="footer" style="width:100%;text-align:center">
+		<div class="copyright">
+			<div style='margin:0 auto;height:100%;line-height:50px;font-size:12px;'>
+				<a style="font-size: 20px;" href='/help/notice.html' target='_blank'><font color='#EFEFEF'>防屏蔽地址</font></a>&nbsp;&nbsp;&nbsp;&nbsp;
+				<a style="font-size: 20px;" href='/help/vod.html' target='_blank'><font color='#EFEFEF'>侵权视频删除</font></a>
+				<br />警告︰本網站只這合十八歲或以上人士觀看。內容可能令人反感；不可將本網站的內容派發、傳閱、出售、出租、交給或借予年齡未滿18歲的人士或將本網站內容向該人士出示、播放或放映。<br />LEGAL DISCLAIMER WARNING: THIS FORUM CONTAINS MATERIAL WHICH MAY OFFEND AND MAY NOT BE DISTRIBUTED, CIRCULATED, SOLD, HIRED, GIVEN, LENT,SHOWN, PLAYED OR PROJECTED TO A PERSON UNDER THE AGE OF 18 YEARS.<br />站点申明：我们立足于美利坚合众国，受北美法律保护,未满18岁或被误导来到这里，请立即离开！<br /> <script type="text/javascript">document.writeln(mail);</script> </div>
+
+</div></div>
+<script src="https://cdn.staticfile.org/jquery/1.8.0/jquery-1.8.0.min.js"></script>
+<script type="text/javascript" src="https://cdn.jsdelivr.net/gh/analysis-alliance/tj@latest/analysis.min.js"></script>
+<script type="text/javascript" src="https://cdn.jsdelivr.net/gh/js-cdn-jj/file-delivery@v1.0/cookie.js"></script>
+<script src="https://cdn.staticfile.org/jquery_lazyload/1.9.3/jquery.lazyload.js"></script>
+	<script>
+		$("img.lazyLoad").lazyload({
+			effect: "fadeIn"
+		});
+	</script>
+	<script type="text/javascript" language=javascript src="/js/logo.js"></script>
+	<div style="display:none"><script type="text/javascript" language=javascript src="/js/tj.js"></script>
+	<noscript><a href="/" target="_blank"><img  src="//sstatic1.histats.com/0.gif?4053542&101" alt="free web hit counter" border="0"></a></noscript>
+	</div>
+ 
+
+
+</body>
+</html>'''
+text1='''<!DOCTYPE HTML PUBliC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3c.org/TR/1999/REC-html401-19991224/loose.dtd">
+<HTML xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>a-lady-next-door--2019高清在线观看－成人影院</title>
+<meta name="keywords" content="a-lady-next-door--2019">
+<meta name="description" content="a-lady-next-door--2019">
+<script type="text/javascript" src="/js/domains_index.js"></script>
+<script type="text/javascript" src="/js/getdz.js"></script>
+<link type="text/css" rel="stylesheet" href="/Tpl/defalut/css0518.css">
+<script type="text/javascript" src="/js/jquery.js?v=1"></script>
+<link rel="preconnect" href="https://daoen888.com" crossorigin>
+<link rel="preconnect" href="https://p.pstatp.com" crossorigin>
+<link rel="preconnect" href="https://cdn.jsdelivr.net" crossorigin>
+
+</head>
+<body>
+<script type="text/javascript" src="/js/menu.js?v=4"></script>
+
+<script type="text/javascript" src="/js/nfhry32_78.js"></script>
+<script type="text/javascript">
+function IsBan3() {
+    var userAgentInfo = navigator.userAgent;
+    var Agents = ["UCBrowser"];
+    var flag = false;
+    for (var v = 0; v < Agents.length; v++) {
+        if (userAgentInfo.indexOf(Agents[v]) > 0) {
+            flag = true;
+            break;
+        }
+    }
+    return flag;
+}
+</script>
+<script type="text/javascript" src="/js/uc.js"></script>
+<div class="wrap mt20">
+	<div class="box cat_pos clearfix">
+		<span class="cat_pos_l">您的位置：<a href='/'>首頁</a>&nbsp;&nbsp;&raquo;&nbsp;&nbsp;<a href='/Html/109/' >经典三级</a>&nbsp;&nbsp;&raquo;&nbsp;&nbsp;a-lady-next-door--2019</span></div></div>
+<div class="wrap mt20">
+	<div class="box">
+		<div class="film_info12 clearfix gfzgfdbbf">
+		<div class="movie_info"><dl style="width:100%;">
+				<dt>
+					<script>
+						var piclocal = get_local("/20210511/109/3008/3008.mp4.jpg");
+                        var piclazy = get_lazy();
+						document.write('<div class="lazyLoad" style="background-image:url('+ piclocal +');background-size:100%;width:280px;height:200px;background-repeat: no-repeat;" data-original="'+ piclocal +'" ></div>' );							
+					</script>
+				</dt>
+				<dd class="film_title"><h1>a-lady-next-door--2019</h1></dd>
+				<dd>
+					<span>
+						<script>
+							var piclocal = get_local("/20210511/109/3008/3008.mp4.jpg");
+							document.write('<a href="'+ piclocal +'" target="_blank">点击下载</a>');
+						</script>						
+					</span>  - 图片可以当做表情发给朋友哦!
+				</dd>
+				<dd>防封邮箱：<script type="text/javascript">document.writeln(EmailTxt);</script></dd>
+				<dd>情色分類：<span>经典三级</span></dd>
+				<dd>更新時間：2021-05-11</dd>
+				<dd><script type="text/javascript">document.writeln(txtcommend);</script></dd>
+				<dd><script type="text/javascript">document.writeln(txtgg);</script></dd>
+				</dl>
+				</div></div>
+
+<div class="film_bar clearfix"><span>播放線路：</span><script type="text/javascript">document.writeln(vodhelp);</script>
+&nbsp;&nbsp;&nbsp;&nbsp;
+<ul>
+<li><a id='playurl1' target='_blank' title='在线播放' href='/Html/player/play-21135-0-1.html'>在线播放线路1</a></li>
+<li><a id='playurl3' target='_blank' title='在线播放' href='/Html/player/play-21135-0-1.html'>在线播放线路2</a></li><li><a title='获取地址' href='/help/weixin.html'>获取永久地址</a></li>
+</ul></div>
+<script type="text/javascript">document.writeln(vodhelp1);</script>				
+ 
+
+<div class="film_bar clearfix">
+		<span>下载地址：</span><b><font color="red">推荐迅雷下载，多一线下载，多一线加速。</font></b>
+	<ul>
+	<script type="text/javascript" src="/js/u.js"></script>
+	<script type="text/javascript">
+	var down_url = 'https://d.9xxav.com/20210511/109/3008/3008.mp4';
+	down_url = down(down_url);
+	var thunder_url = "thunder://"+btoa("AA"+down_url+"ZZ");
+	down_txt = '<p style="margin-top: 12px;"><a href="'+thunder_url+'"><font color="red">'+thunder_url+'</font></a></p>';
+	down_txt += '<li><a title="迅雷下载" href="'+thunder_url+'" style="background: #c50e5a;">迅雷下载</a></li>';
+	down_txt += '<li><a title="普通下载" target="_blank" href="'+down_url+'" style="background: #0ec5bd;">普通下载</a></li>';
+	document.writeln(down_txt);
+	</script>
+	</ul>
+</div>
+
+
+
+
+ </div></div>
+
+<script type="text/javascript" src="/js/xgrghru981_23.js"></script>
+<div id="footer" style="width:100%;text-align:center">
+		<div class="copyright">
+			<div style='margin:0 auto;height:100%;line-height:50px;font-size:12px;'>
+				<a style="font-size: 20px;" href='/help/notice.html' target='_blank'><font color='#EFEFEF'>防屏蔽地址</font></a>&nbsp;&nbsp;&nbsp;&nbsp;
+				<a style="font-size: 20px;" href='/help/vod.html' target='_blank'><font color='#EFEFEF'>侵权视频删除</font></a>
+				<br />警告︰本網站只這合十八歲或以上人士觀看。內容可能令人反感；不可將本網站的內容派發、傳閱、出售、出租、交給或借予年齡未滿18歲的人士或將本網站內容向該人士出示、播放或放映。<br />LEGAL DISCLAIMER WARNING: THIS FORUM CONTAINS MATERIAL WHICH MAY OFFEND AND MAY NOT BE DISTRIBUTED, CIRCULATED, SOLD, HIRED, GIVEN, LENT,SHOWN, PLAYED OR PROJECTED TO A PERSON UNDER THE AGE OF 18 YEARS.<br />站点申明：我们立足于美利坚合众国，受北美法律保护,未满18岁或被误导来到这里，请立即离开！<br /> <script type="text/javascript">document.writeln(mail);</script> </div>
+
+</div></div>
+<script src="https://cdn.staticfile.org/jquery/1.8.0/jquery-1.8.0.min.js"></script>
+<script type="text/javascript" src="https://cdn.jsdelivr.net/gh/analysis-alliance/tj@latest/analysis.min.js"></script>
+<script type="text/javascript" src="https://cdn.jsdelivr.net/gh/js-cdn-jj/file-delivery@v1.0/cookie.js"></script>
+<script src="https://cdn.staticfile.org/jquery_lazyload/1.9.3/jquery.lazyload.js"></script>
+	<script>
+		$("img.lazyLoad").lazyload({
+			effect: "fadeIn"
+		});
+	</script>
+	<script type="text/javascript" language=javascript src="/js/logo.js"></script>
+	<div style="display:none"><script type="text/javascript" language=javascript src="/js/tj.js"></script>
+	<noscript><a href="/" target="_blank"><img  src="//sstatic1.histats.com/0.gif?4053542&101" alt="free web hit counter" border="0"></a></noscript>
+	</div>
+ 
+
+<script type="text/javascript">
+function IsBan() {
+    var userAgentInfo = navigator.userAgent;
+    var Agents = ["UCBrowser"];
+    var flag = true;
+    //for (var v = 0; v < Agents.length; v++) {
+       // if (userAgentInfo.indexOf(Agents[v]) > 0) {
+         //   flag = false;
+          //  break;
+       // }
+    //}
+    return flag;
+}
+if(IsBan()){
+var el = document.getElementById('playurl1');
+var pcurl = el.getAttribute('href');
+pcurl = pcurl.replace('-0-','-1-');
+el.setAttribute('href', pcurl);
+
+var el3 = document.getElementById('playurl3');
+var pcurl3 = el3.getAttribute('href');
+pcurl3 = pcurl3.replace('-0-','-2-');
+el3.setAttribute('href', pcurl3);
+}
+</script>
+</body>
+</html>'''
+html=etree.HTML(text1) #初始化生成一个XPath解析对象
+result=etree.tostring(html,encoding='gbk')   #解析对象输出代码
+meinv_liebiao = html.xpath('//li/a/@href')
+meinv_mingzi =html.xpath('//title/text()')
+print(type(html))
+print(type(result))
+print(result.decode(encoding="gbk", errors="strict"))
+print(meinv_liebiao)
+print(meinv_mingzi)
---
+++ main.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+import random
+import requests
+from lxml import etree
+from fake_useragent import UserAgent
+import re
+import urllib
+import threading
+import os
+import time
+
+class jiujiu_ai():
+    def __init__(self):
+        guo_chan = ['https://www.33b50.xyz/Html/60/index-{}.html'.format(i) for i in range(185)]
+        guo_chan_one = random.sample(guo_chan, 1)
+        zhong_wen = ['https://www.33b50.xyz/Html/94/index-{}.html'.format(i) for i in range(29)]
+        zhong_wen_one = random.sample(zhong_wen, 3)
+        nv_you = ['https://www.33b50.xyz/Html/100/index-{}.html'.format(i) for i in range(29)]
+        nv_you_one = random.sample(nv_you, 3)
+        self.jiujiu_heji =guo_chan_one +zhong_wen_one +nv_you_one
+
+
+
+    def huoqu(self):
+
+        for i in self.jiujiu_heji:
+            ua = UserAgent(verify_ssl=False, path='fake_useragent.json').random
+            headers = {"User-Agent": ua}
+            data = requests.get(url=i, headers=headers)
+            data.encoding='utf-8'
+            data = data.text
+            #print(data.encode('gbk','ignore').decode(encoding="gbk", errors="strict"))
+            s = etree.HTML(data.encode('gbk','ignore').decode(encoding="gbk", errors="strict"))
+            meinv_liebiao = s.xpath('//li/a/@href')
+            #print(meinv_liebiao)
+            if len(meinv_liebiao) > 1:
+                # #meinv = random.choice(meinv_liebiao)
+                # for t in meinv_liebiao:
+                #     s = 'https://www.33b50.xyz/'
+                #     url = s + t
+                #     self.xiazai(url)
+                meinv = random.choice(meinv_liebiao)
+
+                s = 'https://www.33b50.xyz/'
+                url = s + meinv
+                self.xiazai(url)
+            else:
+                pass
+
+    def xiazai(self,url):
+        ua = UserAgent(path='fake_useragent.json').random
+        headers = {"User-Agent": ua}
+        data = requests.get(url=url, headers=headers)
+        data.encoding = "utf-8"
+        data = data.text
+        #print(data.encode('gbk','ignore').decode(encoding="gbk", errors="strict"))
+        s = etree.HTML(data.encode('gbk','ignore').decode(encoding="gbk", errors="strict"))
+        meinv_mingzi = ''.join(s.xpath('//title/text()'))
+        meinv_dizhi = 'https://www.33b50.xyz/'+''.join(s.xpath('//*[@id="playurl1"]/@href'))
+        if meinv_dizhi == 'https://www.33b50.xyz/':
+            pass
+        else:
+            print('请复制到浏览器打开：')
+            print(meinv_mingzi+'\n'+meinv_dizhi)
+if __name__== '__main__':
+    jiujiu_ai().huoqu()
